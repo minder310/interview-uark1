aaefbc1719a5938979661a02e46542cfc20d708f
pierre.K <minder309@gmail.com>
2023-08-23 11:30:06 +0800

    此帳號代審核開通，與密碼判斷完成。

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,20 +8,30 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         // 這裡是驗證規則，'exists:orgs,org_no'是與資料庫串聯並且驗證是否有這個號碼。
+    //         'account' => ['required', 'string', 'max:255', 'exists:users,status,1'],
+    //     ]);
+    // }
     public function show(Request $request)
     {
         $user = DB::table('users')->where('account', $request->account)->first();
         if (empty($user)) {
+            // 帳號不存在
             return '帳號錯誤';
         } else if ($user->status == 0) {
-            return '帳號審核中';
-        } else if (bcrypt($user->password) != $user->password) {
+            // 這是回傳$message的，至前端的方法。並顯示錯誤碼。
+            return redirect()->back()->withErrors(['account' => '此帳號代審核開通']);
+        } else if (!Hash::check($request->password, $user->password)) { //前面這段是laravel的驗證密碼的方法
             return '密碼錯誤';
         }
-        return 'account';
+        return '登入成功';
     }
     /*
     |--------------------------------------------------------------------------
